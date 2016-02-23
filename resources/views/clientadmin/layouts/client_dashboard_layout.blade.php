abfaa95ec429db4444a0cb6aa8418ede44358ef7
Hasif k <hasif.silverbloom@gmail.com>
2016-02-23 17:00:45 +0530

    23/02

--- resources/views/clientadmin/layouts/client_dashboard_layout.blade.php
+++ resources/views/clientadmin/layouts/client_dashboard_layout.blade.php
@@ -24,7 +24,7 @@
 
         <link rel="stylesheet" href="{{asset('assets/clientassets/css/custom.css')}}">
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-      
+
         <script src="{{asset('assets/clientassets/plugins/jQuery/jquery-2.2.0.min.js')}}"></script>
     </head>
     <body class="hold-transition skin-blue sidebar-mini">
@@ -57,70 +57,70 @@
 
 
         </div><!-- ./wrapper -->
-
-        <!-- jQuery 2.1.4 -->
         <script src="{{asset('assets/clientassets/js/custom.js')}}"></script>
+        <script src="{{asset('assets/clientassets/js/custom_script.js')}}"></script>
+        
         <!-- Bootstrap 3.3.5 -->
         <script src="{{asset('assets/clientassets/bootstrap/js/bootstrap.min.js')}}"></script>
         <!-- FastClick -->
+        
         <script src="{{asset('assets/clientassets/plugins/fastclick/fastclick.min.js')}}"></script>
         <!-- AdminLTE App -->
+        
         <script src="{{asset('assets/clientassets/dist/js/app.min.js')}}"></script>
+        
         <!-- Select2 -->
         <script src="{{asset('assets/clientassets/plugins/select2/select2.full.min.js')}}"></script>
-        
+
         <!-- InputMask -->
         <script src="{{asset('assets/clientassets/plugins/input-mask/jquery.inputmask.js')}}"></script>
-         <script src="{{asset('assets/clientassets/plugins/input-mask/jquery.inputmask.date.extensions.js')}}"></script>
+        <script src="{{asset('assets/clientassets/plugins/input-mask/jquery.inputmask.date.extensions.js')}}"></script>
         <script src="{{asset('assets/clientassets/plugins/input-mask/jquery.inputmask.extensions.js')}}"></script>
-        
+
         <!-- date-range-picker -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
         <script src="{{asset('assets/clientassets/plugins/daterangepicker/daterangepicker.js')}}"></script>
-        
+
         <script src="{{asset('assets/clientassets/dist/js/demo.js')}}"></script>
         <script src="{{asset('assets/clientassets/plugins/ckeditor/ckeditor.js')}}"></script>
 
-       
-        <script src="{{asset('assets/clientassets/js/custom_script.js')}}"></script>
-        
         <script>
-            $(function () {
-                //Initialize Select2 Elements
-                $(".select2").select2();
+$(function () {
+    //Initialize Select2 Elements
+    $(".select2").select2();
 
-                //Datemask dd/mm/yyyy
-                $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
-                //Datemask2 mm/dd/yyyy
-                $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
-                //Money Euro
-                $("[data-mask]").inputmask();
+    //Datemask dd/mm/yyyy
+    $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
+    //Datemask2 mm/dd/yyyy
+    $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
+    //Money Euro
+    $("[data-mask]").inputmask();
 
-                //Date range picker
-                $('#reservation').daterangepicker();
-                //Date range picker with time picker
-                $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
-                //Date range as a button
-                $('#daterange-btn').daterangepicker(
-                        {
-                            ranges: {
-                                'Today': [moment(), moment()],
-                                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-                            },
-                            startDate: moment().subtract(29, 'days'),
-                            endDate: moment()
-                        },
-                        function (start, end) {
-                            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-                        }
-                );
-            });
+    //Date range picker
+    $('#reservation').daterangepicker();
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+            {
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                },
+                startDate: moment().subtract(29, 'days'),
+                endDate: moment()
+            },
+            function (start, end) {
+                $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            }
+    );
+});
         </script>
-        <script src="{{asset('assets/clientassets/js/Chart.Core.js')}}"></script>
-        <script src="{{asset('assets/clientassets/js/Chart.Doughnut.js')}}"></script>
+<!--        <script src="{{asset('assets/clientassets/js/Chart.Core.js')}}"></script>
+        <script src="{{asset('assets/clientassets/js/Chart.Doughnut.js')}}"></script>-->
     </body>
 </html>
