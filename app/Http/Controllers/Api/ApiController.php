5a505f11ba93453f3816a1ea1459338896782ffa
Hasif k <hasif.silverbloom@gmail.com>
2016-03-07 18:25:25 +0530

    07/03

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -99,6 +99,7 @@ class ApiController extends Controller {
   $result=json_decode(file_get_contents('php://input'),true);
   var_dump($result);
   var_dump($result['name']);
+  return $result['name'];
         //$admin = Auth::user()->id;
 //        $obj = new Model\Booking;
 //        $obj->admin_id = $id;
