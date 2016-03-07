f8c5a76f2e01c1a054bf0a1296b937b5bf08556a
Hasif k <hasif.silverbloom@gmail.com>
2016-03-07 16:38:25 +0530

    07/03

--- public/assets/clientassets/js/custom.js
+++ public/assets/clientassets/js/custom.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+     $.ajaxSetup({
+        headers: { 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content') }
+    });
     $('.to_ck').each(function () {
 
         CKEDITOR.replace($(this).attr('id'));
