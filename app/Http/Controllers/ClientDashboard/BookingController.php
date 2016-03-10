20b640b97d67275b3667519b17678e1f63aab85b
Hasif k <hasif.silverbloom@gmail.com>
2016-03-10 11:20:43 +0530

    10/03

--- app/Http/Controllers/ClientDashboard/BookingController.php
+++ app/Http/Controllers/ClientDashboard/BookingController.php
@@ -88,38 +88,38 @@ class BookingController extends Controller {
         // prep the bundle
         echo $mssg; 
         exit;
-        $msg = array
-            (
-            'message' => $mssg,
-            'title' => "Booking Status",
-            'subtitle' => 'This is a subtitle. subtitle',
-            'tickerText' => 'Ticker text here...Ticker text here...Ticker text here',
-            'vibrate' => 1,
-            'sound' => 1,
-            'largeIcon' => 'large_icon',
-            'smallIcon' => 'small_icon'
-        );
-        $fields = array
-            (
-            'registration_ids' => $ids[1],
-            'data' => $msg
-        );
-
-        $headers = array
-            (
-            'Authorization: key=' . API_ACCESS_KEY,
-            'Content-Type: application/json'
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-        $result = curl_exec($ch);
-        curl_close($ch);
+//        $msg = array
+//            (
+//            'message' => $mssg,
+//            'title' => "Booking Status",
+//            'subtitle' => 'This is a subtitle. subtitle',
+//            'tickerText' => 'Ticker text here...Ticker text here...Ticker text here',
+//            'vibrate' => 1,
+//            'sound' => 1,
+//            'largeIcon' => 'large_icon',
+//            'smallIcon' => 'small_icon'
+//        );
+//        $fields = array
+//            (
+//            'registration_ids' => $ids[1],
+//            'data' => $msg
+//        );
+//
+//        $headers = array
+//            (
+//            'Authorization: key=' . API_ACCESS_KEY,
+//            'Content-Type: application/json'
+//        );
+//
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
+//        curl_setopt($ch, CURLOPT_POST, true);
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+//        $result = curl_exec($ch);
+//        curl_close($ch);
     }
 
 }
