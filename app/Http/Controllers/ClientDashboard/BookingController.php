87e32afeeb16a3736d5733d7f41afd5dcd8ce88f
Hasif k <hasif.silverbloom@gmail.com>
2016-03-10 11:53:33 +0530

    10/03

--- app/Http/Controllers/ClientDashboard/BookingController.php
+++ app/Http/Controllers/ClientDashboard/BookingController.php
@@ -86,7 +86,7 @@ class BookingController extends Controller {
         else 
             $mssg = "Sorry..Your appoinment booking is Pending, it will be approved shortly";
         // prep the bundle
-        
+        return($ids[1]);
         $msg = array
             (
             'message' => $mssg,
