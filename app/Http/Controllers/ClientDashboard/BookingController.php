529b6f59a2d00dce92baca9ba98ef03625a1319a
Hasif k <hasif.silverbloom@gmail.com>
2016-03-14 16:57:41 +0530

    14/03

--- app/Http/Controllers/ClientDashboard/BookingController.php
+++ app/Http/Controllers/ClientDashboard/BookingController.php
@@ -128,7 +128,7 @@ class BookingController extends Controller {
 //$tHost = 'gateway.push.apple.com';
             $tPort = 2195;
 // Provide the Certificate and Key Data.
-            $tCert = realpath('../public/assets/clientassets/') . '/' . 'pushcert.pem';
+            $tCert = realpath('../../public/assets/clientassets/') . '/' . 'pushcert.pem';
             //$tCert = asset('assets/clientassets/') . '/' . 'pushcert.pem';
 // Provide the Private Key Passphrase (alternatively you can keep this secrete
 // and enter the key manually on the terminal -> remove relevant line from code).
