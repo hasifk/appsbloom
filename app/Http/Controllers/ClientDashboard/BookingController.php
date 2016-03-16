93ce652c4bd65ac7b7fdd64e1cd9c605e1123822
Hasif k <hasif.silverbloom@gmail.com>
2016-03-16 12:58:21 +0530

    16/03

--- app/Http/Controllers/ClientDashboard/BookingController.php
+++ app/Http/Controllers/ClientDashboard/BookingController.php
@@ -124,7 +124,7 @@ class BookingController extends Controller {
             curl_close($ch);
         } else {
 // Provide the Host Information.
-              $tHost = 'gateway.sandbox.push.apple.com';
+            $tHost = 'gateway.sandbox.push.apple.com';
             //$tHost = 'gateway.push.apple.com';
             $tPort = 2195;
 // Provide the Certificate and Key Data.
@@ -162,24 +162,24 @@ class BookingController extends Controller {
 // Remove this line if you would like to enter the Private Key Passphrase manually.
             stream_context_set_option($tContext, 'ssl', 'passphrase', $tPassphrase);
 // Open the Connection to the APNS Server.
-            $tSocket = stream_socket_client('ssl://' . $tHost . ':' . $tPort, $error, $errstr,30, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $tContext);
+            $tSocket = stream_socket_client('ssl://' . $tHost . ':' . $tPort, $error, $errstr, 30, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $tContext);
 // Check if we were able to open a socket.
             if (!$tSocket)
                 exit("APNS Connection Failed: $error $errstr" . PHP_EOL);
 // Build the Binary Notification.
             $tMsg = chr(0) . chr(0) . chr(32) . pack('H*', $tToken) . pack('n', strlen($tBody)) . $tBody;
 
-      // Ensure that blocking is disabled
-stream_set_blocking($tSocket, 0);
+            // Ensure that blocking is disabled
+            stream_set_blocking($tSocket, 0);
             //stream_set_blocking($tSocket, 0);
 // Send the Notification to the Server.
-            $tResult = fwrite($tSocket, $tMsg,strlen($tMsg));
-            
-           // $tResult = fwrite($tSocket, $tMsg);
-if ($tResult)
-return 'Delivered Message to APNS' . PHP_EOL;
-else
-return 'Could not Deliver Message to APNS' . PHP_EOL;
+            $tResult = fwrite($tSocket, $tMsg, strlen($tMsg));
+
+            // $tResult = fwrite($tSocket, $tMsg);
+//            if ($tResult)
+//                return 'Delivered Message to APNS' . PHP_EOL;
+//            else
+//                return 'Could not Deliver Message to APNS' . PHP_EOL;
             //Close the Connection to the Server.
             fclose($tSocket);
         }
