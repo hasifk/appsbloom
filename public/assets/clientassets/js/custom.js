e7085a49313d68c424fcce9c3b32adf1e620fba4
Hasif k <hasif.silverbloom@gmail.com>
2016-03-16 16:03:10 +0530

    16/03

--- public/assets/clientassets/js/custom.js
+++ public/assets/clientassets/js/custom.js
@@ -552,7 +552,7 @@ function selectall() {
 function selectCheck(obj)
 {
     var i = $( ".cck" ).length;
-    alert($( ".cck" )[1]);
+    alert($( ".cck" )[1].val());
 //    for (var k = 0; k < i; k++)
 //    {
 //        if (document.frm.elements[k].className == fieldName)
