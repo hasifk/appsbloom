9fa6b03c971ce02c92c095bbd833fe0e7370352a
Hasif k <hasif.silverbloom@gmail.com>
2016-03-17 14:09:35 +0530

    17/03

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -127,11 +127,13 @@ public function AppointmentChecking(Request $request) {
         $result = json_decode(file_get_contents('php://input'));
         $date = date('d-m-Y',strtotime($result->date));
         $time=explode(":",$result->time);
-        $booking = Model\Booking::where('admin_id', $result->admin_id)->where('date', 'like', $date. " " .$time[0].":".$time[1]."%")->get();
-        if(count($booking)>0)
-            return "1";
-        else 
-            return "0";
+//        $booking = Model\Booking::where('admin_id', $result->admin_id)->where('date', 'like', $date." " .$time[0].":".$time[1]."%")->get();
+//        if(count($booking)>0)
+//            return "1";
+//        else 
+//            return "0";
+        return $time[0];
+        
     }
     public function InsertFanwall(Request $request) {
         $admin = Auth::user()->id;
